432790fb3b70f96f330c528646b05f9c0e13784c
RaghavaBandaru <raghavabandaru854@gmail.com>
2026-01-15 11:20:43 +1000

    update terraform

--- terraform-review-agent/terraform/backend.tf
+++ terraform-review-agent/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "pravesh-terraform-mario-state"
+    bucket       = "raghava-ai-devops-state"
     key          = "ecs/serverless/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
